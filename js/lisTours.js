46f9e819620a6c9d709bc53a666ca0cd33964965
Alex Rice <agr@ncgr.org>
2016-01-29 13:52:21 -0600

    Cleanup cookie after tour finished. Js code cleanup.

---
+++ js/lisTours.js
@@ -0,0 +1,44 @@
+/* 
+ * lisTours
+ * - use a cookie to track which, if any, tour the user is navigating.
+ * - call hopscotch with the javascript tour definition object.
+ * - remove the cookie when tour ends.
+ */
+
+var lisTours = {};
+
+(function(){
+
+  this.cookieId = 'lis-multipage-tour';
+  this.tourId = Cookies.get(this.cookieId);
+  
+  if(this.tourId !== undefined) {
+    startOrResumeTour(this.tourId);
+  }
+  
+  this.go = function(tourId) {
+    this.tourId = tourId;
+    Cookies.set(this.cookieId, tourId);
+    startOrResumeTour(tourId);
+  }
+
+  function startOrResumeTour(tourId) {
+    jQuery.getScript('/lis-tours/' + tourId + '/js', function() {
+      if(! tour) {
+	console.log('failed to load tour: ' + tourId);
+	return;
+      }
+      if(tour.onEnd) {
+	console.log('warning: will not clean up cookie after tour ending.');
+      }
+      else {
+	tour.onEnd = function() { Cookies.remove(lisTours.cookieId); };
+      }
+      hopscotch.startTour(tour);
+    });
+  }
+  
+}.call(lisTours));
+
+
+
