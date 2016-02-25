8a902ff03d789ca2d821b0ed807a5d2b1775da5e
User Mhadres <mhadres@lis-dev.agron.iastate.edu>
2016-02-25 17:49:17 -0600

    Safekeeping before update

--- tours/tour1.js
+++ tours/tour1.js
@@ -1,28 +1,88 @@
 var tour = {
   'id': 'tour1',
+	
+	function($) {
+	var tourState = hopscotch.getState(tourId);
+	if(tourState !== null && tourState != undefined) {
+		return; }
+	$(document).ready(waitForiFrame);
+console.log("waited for it");
+		function waitForiFrame() {
+		if(!$('#frameviewer').length ||!('$("#frameviewer").contentWindow.document.body.innerHTML').length){
+		setTimeout(waitForiFrame, 2000);
+		return;}}},
+
   'steps' : [ {
-    'title': 'hello tour 1',
-    'content': 'This is the tour launching page.',
+    'title': 'Welcome to LIS!',
+    'content': 'Clicking "next" will take you to our homepage.',
     'placement': 'bottom',
     'target': 'site-name',
+    'multipage': 'true',
     'onNext': function() {
-      window.location = '/home';
-    }
-  }, {
-    'title': 'hello tour 2',
-    'content': 'This is the home page.',
+      window.location = '/home'}
+}, {
+    'title': 'Some guidance',
+    'content': 'This tour will provide an example for how one could use our tools to improve their crops.',
     'placement': 'bottom',
-    'target': 'site-name',
+    'target': jQuery("[href='/search/qtl']")[0] || 'site-name',
+    'multipage': 'true',
     'onNext' : function() {
-      window.location = '/organism/Phaseolus/vulgaris';
+      window.location = '/search/qtl';
     },
-  }, {
-    'title': 'hello tour3',
-    'content': 'Frijoles!',
+}, {
+    'title': 'Given: A QTL.',
+    'content': 'Let us assume we are working with a "seed yield" QTL.',
     'placement': 'bottom',
-    'target': jQuery('.tripal-organism-img')[0] || 'site-name',
-  }
- ]
+    'target': jQuery("[href='/feature/Phaseolus/vulgaris/QTL/phavu.Seed-yield-2-5']")[0] || 'site-name',
+    'onNext' : function() {
+	window.location = '/feature/Phaseolus/vulgaris/QTL/phavu.Seed-yield-2-5';}
+},{
+    'title': 'The Overview',
+    'content': "Here you can find out more about this trait's  place on the chromosome.",
+    'placement': 'top',
+    'target': jQuery("[href='?pane=qtl_details']") [0] || 'site-name',
+    'onNext': function() {
+	window.location = '/feature/Phaseolus/vulgaris/QTL/phavu.Seed-yield-2-5#pane=qtl_details'}
+},{
+    'title': 'Nearest Marker',
+    'content': 'This is the closest known gene to the segment of DNA that contributes to the seed yield phenotype. Since we know that neighboring alleles tend to be inherited together, we can use markers to find specific traits in chromosomes.',
+    'placement': 'top',
+    'target': jQuery("[href='/node/1360413']")[0] || 'site-name',
+    'multipage': 'true',
+    'onNext': function() {
+	window.location= '/node/1360783#pane=marker'}
+},{
+    'title': 'Marker Overview',
+    'content': 'The "Overview" links to articles and descriptions of the marker.To find this marker on the phaseolus map, we can look under "Marker Positions".',
+    'target': jQuery("[href='?pane=positions'")[0],
+    'placement': 'bottom',
+    'multipage': 'true',
+    'onNext': function() {
+	window.location='/node/1360783?pane=positions'}
+},{
+    'title': 'Genome Browser',
+    'content': 'The linked Gbrowse map will show you annotations on the genome',
+    'target': jQuery("[href='/gbrowse_phavu1.0?query=ref=Pv04;start=15056944;stop=15058034;add=Pv04+Marker+BM199+15056944..15057534;h_feat=BM199@yellow;style=Marker+bgcolor=red']")[0],
+    'placement': 'left', 
+    'multipage': 'true',
+    'onNext': function() {
+	window.location='/gbrowse_phavu1.0?query=ref=Pv04;start=15056944;stop=15058034;add=Pv04+Marker+BM199+15056944..15057534;h_feat=BM199@yellow;style=Marker+bgcolor=red'; 
+        //function test(){ console.log(jQuery("#frameviewer").contents());}
+	setTimeout(this, [1000]);}
+},{
+    'title': 'Gbrowse',
+    'content': 'Allows you to visualize syntenic relationships between legumes of your choice. The red bar is the lowest flanking marker for "seed yield".',
+    'target': 'site-name',
+    'placement': "bottom"
+},{
+    'title': 'Tweaking your parameters',
+    'content': 'There are many ways to change your settings, such as rearranging and adding tracks, zooming in or out, and changing specific visibilities.',
+    'target': jQuery("frameviewer").contents().getElementsById("#panels")[0],  //(".field-items")[0],
+    'placement': 'top'
+}  
+
+ ],
+showPrevButton: true,
 };
 
 
--- tours/tour2.js
+++ tours/tour2.js
@@ -1,26 +1,47 @@
 var tour = {
   'id': 'tour2',
   'steps' : [ {
-    'title': 'hello tour 2',
-    'content': 'This is the tour launching page.',
+    'title': 'Welcome to the Legume Information System',
+    'content': 'Click "next" to go to the Homepage.',
     'placement': 'bottom',
     'target': 'site-name',
+    'multipage': 'true',
     'onNext': function() {
-      window.location = '/home';
-    }
+     window.location = '/home';}
   }, {
-    'title': 'hello tour 2..',
-    'content': 'This is the home page.',
+    'title': 'Gene Contexts',
+    'content': 'Let us begin by searching for a particular mungbean gene, Vradi08g22420.1.',
     'placement': 'bottom',
-    'target': 'site-name',
+    'target': jQuery('#genesearchid')[0],
+    'multipage': 'true',
     'onNext' : function() {
-      window.location = '/organism/Vigna/unguiculata';
-    },
+      window.location = '/search/gene';}
   }, {
-    'title': 'hello tour 2...',
-    'content': 'Cowpeas!',
+    'title': 'Gene Search',
+    'content': 'Let us look for Vradi08g22420.1.',
     'placement': 'bottom',
-    'target': jQuery('.tripal-organism-img')[0] || 'site-name',
-  }
- ]
-};
+    'target': jQuery('#edit-name')[0] || 'site-name',
+    'multipage': 'true',
+    'onNext': function() {
+	window.location='/search/gene?name=Vradi08g22420';}
+  }, {
+    'title': 'Input Gene name',
+    'content': "We've done it for you. Now we want to see this gene's phylogenetic relationships.",
+    'placement': 'bottom',
+    'target': jQuery("[href='/chado_gene_phylotree_v2/Vradi08g22420.Vradi.ver6']")[0],
+    'multipage': 'true',
+    'onNext': function() {
+	window.location='/chado_phylotree/phytozome_10_2.59155167?hilite_node=vigra.Vradi08g22420.1';}
+   }, {
+    'title': 'Phylotree',
+    'content': 'Here is our gene again, surrounded by its cousins.',
+    'target': jQuery('.hilite-node')[0],
+    'placement': 'right'
+   }, {
+    'title':  'More Info',
+    'content': 'Click here for a tour that focuses on the features of this page.',
+    'target': jQuery(".button phylogeny-help phylogeny-help-btn")[0],
+    'placement': 'bottom'
+   }   
+
+]};
