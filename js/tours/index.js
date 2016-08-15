2f26a44dc5c7a1d6edbd58d5d26b57da7ef1716d
Alex Rice <agr@ncgr.org>
2016-08-15 11:25:32 -0500

    Remove development tours from index.

--- js/tours/index.js
+++ js/tours/index.js
@@ -6,8 +6,11 @@
 
 require('./qtl-search-to-gbrowse.js');
 require('./gene-to-phylotree-to-context-viewer.js');
-require('./blast-demo.js');
-require('./protein-domain.js');
 require('./phylotree.js');
 require('./genome-context-viewer.js');
 require('./germplasm-map.js');
+
+// under development:
+
+// require('./blast-demo.js');
+// require('./protein-domain.js');
