b0d8a0ce4fa202751e11ec29ea20383df51e5216
Andrew Farmer <adf@ncgr.org>
2016-08-26 19:17:56 -0500

    initial work on getting this upgraded, still WIP and not ready for release

--- js/tours/blast-demo.js
+++ js/tours/blast-demo.js
@@ -6,66 +6,76 @@
 
   var $ = jQuery;
 
-  //Blast tour
-  tourId = "tour3"
-
   var tour = new Tour({
     name: "blast-demo",
+    orphan: true,
+    debug: true,
     steps: [
       {
 	title: 'BLAST Tour: Welcome to LIS',
 	content: 'Click "next" to go to the homepage',
-	target: 'site-name',
+	element: '#site-name',
 	placement: 'right',
-	multipage: true,
-	onNext: function() {
-	  lisTours.location('/home');}
       }, {
 	title: 'BLAST Tour: Search your sequence against our database',
+        path: '/home',
 	content: 'We have BLAST on our webpage for your legume-browsing convenience.',
-	target: jQuery("[href='/blast']")[1],
+	element: jQuery("[href='/blast']")[1],
 	placement: 'bottom',
-	multipage: true,
-	onNext: function() {
-	  lisTours.location('/blast');}
+        reflex: 'true'
       }, {
 	title: 'BLAST Tour: Protein BLAST',
 	content: 'Pick the version of the blast algorithm that is appropriate given the type (nucleotide or protein) of input sequence you have and the type ofthe sequences in the database you want to search.  We will use blastp to search one of the LIS protein databases with a protein query sequence.',
-	target: jQuery("[href='/./blast/protein/protein']")[0],
-	placement: 'bottom',
-	multipage: true,
-	onNext: function() {
-	  lisTours.location('/blast/protein/protein');}
+	path: '/blast',
+	element: jQuery("[href='/./blast/protein/protein']")[0],
+	placement: 'bottom'
       }, {
 	title: 'BLAST Tour: Begin the Alignment',
 	content: 'We will be using the sample sequence, so tick the checkbox to display it.',
-	target: jQuery('#edit-example-sequence')[0],
+	element: jQuery('#edit-example-sequence')[0],
+	path: '/blast/protein/protein',
 	placement: 'top',
-	showNextButton: false,
-	nextOnTargetClick: true
-	//onNext: function() {
-	//	jQuery('#edit-example-sequence').click();}//trigger('click');}
+	onShown: function(tour) {
+          $('.popover-navigation div').hide();},
+	reflex: true
       }, {
 	title: 'BLAST Tour: Select database',
 	content: 'Select the Cicer arietinum CDC Frontier database to compare sequence against.',
-	target: 'edit-select-db',
+	element: '#edit-select-db',
 	placement: 'top',
-	nextOnTargetClick: true,
-        showNextButton: false,
-	//onNext: function() {
-	//	lisTours.location()='/blast/report/ODE2#loaded/';}
+	reflex: true,
+	 onShown: function(tour) {
+          $('.popover-navigation div').hide();},
       }, { 
 	title: 'BLAST Tour: Blast it',
 	content: 'Click the "BLAST" button to continue to results.',
-	target: jQuery('#edit-submit')[0],
+	element: jQuery('#edit-submit')[0],
 	placement: 'bottom',
-	multipage: true,
-        showNextButton: false,
-	nextOnTargetClick: true
+        reflex: true,
+	onShown: function(tour) {
+          $('.popover-navigation div').hide();},
+      },{
+        title: 'Gene Tour: Gene Search',
+        content: 'Please be patient, as we wait for the query results to be returned...',
+        placement: 'top',
+        onShown: function(tour) {
+          $('.popover-navigation div').hide();
+          // wait for dynamic content with a loading dialog.
+          var deferred = lisTours.waitForContent(
+            tour,
+            function() {
+              return $('tr.views-row-first td.views-field-description:contains("'+FOCUS_GENE_FUNCTION+'")')[0];
+            }, 40000);
+          // advance automatically to next step when done loading
+          deferred.then(function() {
+            tour.next();
+          });
+          return deferred.promise();
+        }
       }, {
 	title: 'BLAST Tour: Results',
 	content: 'The E-value, or expected value, describes the probability that the results are by chance. A lower e-value suggests that the two sequences are more identical, and a "0" suggests that there is no possibility that these are two random sequences, they must be identical.',
-	target: jQuery('.evalue')[0],
+	element: jQuery('.evalue')[0],
 	placement: 'top',
 	arrowOffset: '250',
 	xOffset: '-230',
@@ -75,64 +85,61 @@
 	title: 'BLAST Tour: Positive Score',
 	content: 'This is the percentage of positive scores that came up on the substitution matrix.',
 	delay: '400',
-	target: jQuery('.positive')[0],
+	element: jQuery('.positive')[0],
 	placement: 'bottom'
       }, {
 	title: 'BLAST Tour: Identity',
 	content: 'This scores how alike the two sequences are.',
-	target: jQuery('.identity')[0],
+	element: jQuery('.identity')[0],
 	placement: 'bottom'
       }, {
 	title: 'BLAST Tour: Target Information',
-	content: 'Clicking the target name will take you to that gene\'s GBrowse page, where you can see its synteny with other legumes.',
-	target: jQuery("#blast_report > tbody > tr:nth-child(1) > td.hit > a")[0],
+	content: 'Clicking the element name will take you to that gene\'s GBrowse page, where you can see its synteny with other legumes.',
+	element: jQuery("#blast_report > tbody > tr:nth-child(1) > td.hit > a")[0],
 	placement: 'bottom'
       }, {
 	title: 'BLAST Tour: Continuing',
-	content: 'We can learn more about our chickpea target, Ca_09040, by entering it into a gene search query.',
-	target: jQuery("[href='/search']")[0],
+	content: 'We can learn more about our chickpea element, Ca_09040, by entering it into a gene search query.',
+	element: jQuery("[href='/search']")[0],
 	placement: 'bottom',
-	multipage: true,
 	onNext: function() {
 	  lisTours.location('/search/gene?name=Ca_09040');}
       }, {
 	title: 'BLAST Tour: Our Chickpea Gene',
 	content: 'Searching for gene and gene family information',
-	target: 'edit-name',
+	element: 'edit-name',
 	placement: 'bottom'
       }, {
 	title: 'BLAST Tour: Using the Gene Basket',
 	content: 'Click "Add to Basket" if you would like to save copies of mRNA sequences to compare.',
-	target: jQuery('#ajax-link')[0],
+	element: jQuery('#ajax-link')[0],
 	placement: 'top',
 	arrowOffset: '230',
 	xOffset: '-200'
       }, {
 	title: 'BLAST Tour: Gene Info',
 	content: 'Let\'s gather some more information on our gene',
-	target: jQuery("#block-system-main > div > div > div.view-content > table > tbody > tr.odd.views-row-first > td.views-field.views-field-name.active > a")[0],
+	element: jQuery("#block-system-main > div > div > div.view-content > table > tbody > tr.odd.views-row-first > td.views-field.views-field-name.active > a")[0],
 	placement: 'top',
-	multipage: true,
 	onNext: function() {
 	  lisTours.location('/feature/Cicer/arietinum/gene/Ca_09040_gene');}
       }, {
 	title: 'BLAST Tour: GBrowse',
 	content: 'Here we can see our chickpea gene and choose tracks to add to the visualization.',
 	delay: '400',
-	target: jQuery('#frameviewer') [0],
+	element: jQuery('#frameviewer') [0],
 	placement: 'top',
       }, {
 	title: 'BLAST Tour: Let\'s look at some more genes',
 	content: 'We can also find related genes in the phylotree viewer.',
-	target: jQuery("#tripal_feature-table-base > tbody > tr:nth-child(4) > td > a")[0],
+	element: jQuery("#tripal_feature-table-base > tbody > tr:nth-child(4) > td > a")[0],
 	placement: 'top',
-	multipage: true,
 	onNext: function() {
 	  lisTours.location('/chado_phylotree/phytozome_10_2.59198402?hilite_node=cicar.Ca_09040');}
       }, {
 	title: 'BLAST Tour: Family Tree',
 	content: 'Here is Ca_09040 next to a closely related barrel medick gene.',
-	target: function() {
+	element: function() {
 	  return jQuery('.hilite-node')[0]},
 	placement: 'right',
 	yOffset: '-22'
