d91ae6852641897f71e370cf030b2b9d961deece
adf-ncgr <adf@ncgr.org>
2019-12-23 13:15:22 -0700

    maybe dist really is magical

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,11 @@ module.exports = {
     main: './js/index.js'
   },
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'dist'),
     // the publicPath, unfortunately does not support relative
     // urls. this absolute path is required for webpack's lazy chunk
     // loading to work.
-    publicPath: '/static/grin_app/js/node_modules/lis_tours/build/',
+    publicPath: '/static/grin_app/js/node_modules/lis_tours/dist/',
     filename: 'bundle.js',
     chunkFilename: 'part-[chunkhash].js'
   },
